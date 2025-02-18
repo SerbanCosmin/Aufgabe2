ffdc0cf794c8aa7dd03bcfcf1944de7810b2b2d8
SerbanCosmin <cosminserban2003@gmail.com>
2025-02-18 10:54:02 +0200

    repositories

---
+++ src/Repository/IRepository.java
@@ -0,0 +1,17 @@
+package Repository;
+
+import java.util.List;
+
+/**
+ * Repository interface for CRUD functionalities
+ *
+ * @param <T> Object type
+ * @param <ID> Object identification parameter type
+ */
+public interface IRepository<T, ID> {
+    void add(T entity);
+    void delete(ID entity);
+    void update(T entity1, T entity2);
+    T get(ID entity);
+    List<T> getAll();
+}
